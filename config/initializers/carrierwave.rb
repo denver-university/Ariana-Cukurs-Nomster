f1701c05ed42e137e90668ea00479230b588d054
acukurs77 <ariana.cukurs1@gmail.com>
2019-08-01 21:02:19 +0000

    Lesson 39, fixed security issue and changed file location to :fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,9 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIATCH4W2AUSV4HVRX4"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["arianacukurs"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
 
